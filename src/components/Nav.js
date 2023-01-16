17b9cc445df38c2281763ed85f822e71d5fb50ff
KIJUNG-CHAE <daiywlf5251@gmail.com>
2023-01-16 21:24:39 +0900

    Feat: Navigation Bar

---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window.scrollY", window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Neflex logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+};
+
+export default Nav;
